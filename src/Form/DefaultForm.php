7987be684af8834fa58820f4d7d9b26a2cf27b19
Matt Berry <mattberry23@gmail.com>
2019-03-10 18:14:20 -0700

    cleaned up admin form

--- src/Form/DefaultForm.php
+++ src/Form/DefaultForm.php
@@ -15,16 +15,20 @@ class DefaultForm extends ConfigFormBase {
      * {@inheritdoc}
      */
     protected function getEditableConfigNames() {
+
         return [
             'rtd_cards.settings',
         ];
+
     }
 
     /**
      * {@inheritdoc}
      */
     public function getFormId() {
+
         return 'rtd_cards_default_form';
+
     }
 
     /**
@@ -33,7 +37,9 @@ class DefaultForm extends ConfigFormBase {
     public function buildForm(array $form, FormStateInterface $form_state) {
 
         if ($form_state->has('page_num') && $form_state->get('page_num') == 2) {
+
             return self::rtdCardsPageTwo($form, $form_state);
+
         }
 
         $form_state->set('page_num', 1);
@@ -48,7 +54,7 @@ class DefaultForm extends ConfigFormBase {
             '#type' => 'radios',
             '#options' => $content_types,
             '#title' => $this->t('What content type to use?'),
-            '#description' => 'Which content type to use to display cards',
+            '#description' => 'Which content type to use to display cards?',
             '#default_value' => isset($content_type) ? $content_type : $form_state->getValue('content_type'),
             '#required' => TRUE,
         ];
@@ -62,40 +68,30 @@ class DefaultForm extends ConfigFormBase {
             '#button_type' => 'primary',
             '#value' => $this->t('Next'),
             // Custom submission handler for page 1.
-            '#submit' => ['::rtdCardsFormNextSubmit'],
-            // Custom validation handler for page 1.
-            //'#validate' => ['::fapiExampleMultistepFormNextValidate'],
+            '#submit' => ['::rtdCardsFormNextSubmit']
         ];
 
         return $form;
 
-        //return parent::buildForm($form, $form_state);
-
     }
 
     public function rtdCardsFormNextSubmit(array &$form, FormStateInterface $form_state) {
+
         $form_state
-            ->set('page_values', [
-                // Keep only first step values to minimize stored data.
-                'content_type' => $form_state->getValue('content_type')
-            ])
             ->set('page_num', 2)
-            // Since we have logic in our buildForm() method, we have to tell the form
-            // builder to rebuild the form. Otherwise, even though we set 'page_num'
-            // to 2, the AJAX-rendered form will still show page 1.
+            // rebuild form so AJAX-rendered form will show correct page
             ->setRebuild(TRUE);
+
     }
 
     public function rtdCardsPageTwo(array &$form, FormStateInterface $form_state) {
-
-        //$page_values = $form_state->getValue('page_values');
+        
         $content_type = $form_state->getValue('content_type');
 
         $fields = $this->getFields($content_type);
 
         $available_fields = $this->sortFields($fields);
 
-        $t = 1;
         $form['content_type'] = [
             '#type' => 'hidden',
             '#default_value' => $content_type
@@ -104,23 +100,20 @@ class DefaultForm extends ConfigFormBase {
         $form['text_field'] = [
             '#type' => 'radios',
             '#options' => $available_fields['text_fields'],
-            '#title' => $this->t('Which text field do you want to use?'),
-            //'#default_value' => $form_state->getValue('color', ''),
+            '#title' => $this->t('Which text field do you want to use?')
         ];
 
         $form['image_field'] = [
             '#type' => 'radios',
             '#options' => $available_fields['image_fields'],
-            '#title' => $this->t('Which image field do you want to use?'),
-            //'#default_value' => $form_state->getValue('color', ''),
+            '#title' => $this->t('Which image field do you want to use?')
         ];
 
         $form['taxonomy_field'] = [
             '#type' => 'radios',
             '#options' => $available_fields['taxonomy_fields'],
             '#title' => $this->t('Which taxonomy field do you want to use?'),
-            '#required' => TRUE,
-            //'#default_value' => $form_state->getValue('color', ''),
+            '#required' => TRUE
         ];
 
         $form['back'] = [
@@ -128,36 +121,29 @@ class DefaultForm extends ConfigFormBase {
             '#value' => $this->t('Back'),
             // Custom submission handler for 'Back' button.
             '#submit' => ['::rtdCardsPageTwoBack'],
-            // We won't bother validating the required 'color' field, since they
-            // have to come back to this page to submit anyway.
             '#limit_validation_errors' => [],
         ];
 
-        /*$form['submit'] = [
-            '#type' => 'submit',
-            '#button_type' => 'primary',
-            '#value' => $this->t('Submit'),
-        ];*/
-
         return parent::buildForm($form, $form_state);
+
     }
 
     public function rtdCardsPageTwoBack(array &$form, FormStateInterface $form_state) {
+
         $form_state
-            // Restore values for the first step.
-            ->setValues($form_state->get('page_values'))
             ->set('page_num', 1)
-            // Since we have logic in our buildForm() method, we have to tell the form
-            // builder to rebuild the form. Otherwise, even though we set 'page_num'
-            // to 1, the AJAX-rendered form will still show page 2.
+            // rebuild form so AJAX-rendered form will show correct page
             ->setRebuild(TRUE);
+
     }
 
     /**
      * {@inheritdoc}
      */
     public function validateForm(array &$form, FormStateInterface $form_state) {
+
         parent::validateForm($form, $form_state);
+
     }
 
     /**
@@ -173,15 +159,21 @@ class DefaultForm extends ConfigFormBase {
             ->save();
 
         parent::submitForm($form, $form_state);
+
     }
 
     private function getContentTypes() {
 
         $node_types = NodeType::loadMultiple();
+
         $content_types = [];
+
         foreach ($node_types as $node_type) {
+
             $content_types[$node_type->get('type')] = $node_type->label();
+
         }
+
         return $content_types;
 
     }
